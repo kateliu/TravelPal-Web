ac53d5afef9f7e1c8c23ca0e23581b9bc38acfdc
Owen Chu <owenchu@gmail.com>
2013-09-21 21:36:41 -0700

    Copy getEventExpense() from travelCtrl to eventCtrl.

--- public/javascripts/controllers/eventCtrl.js
+++ public/javascripts/controllers/eventCtrl.js
@@ -5,15 +5,39 @@ travelpalApp.controller('eventCtrl', ['$scope', '$routeParams', 'angularFire', '
     console.log('eventCtrl: ' + $routeParams.eventId);
 
     var eventId = $routeParams.eventId;
+    var eventReady = false;
 
     $scope.event = {};
     $scope.travel = {};
     $scope.users = {};
 
     angularFire(firebaseRef.event(eventId), $scope, 'event').then(function() {
+      eventReady = true;
+
       angularFire(firebaseRef.travel($scope.event.travel), $scope, 'travel');
     });
 
     angularFire(firebaseRef.users, $scope, 'users');
+
+    $scope.getEventExpense = function() {
+      if (!eventReady) {
+        return 0;
+      }
+
+      var expenses = $scope.event.expenses;
+      var totalExpense = 0;
+      var expenseId;
+      var payers;
+      var payerId;
+
+      for (expenseId in expenses) {
+        payers = expenses[expenseId].payers;
+        for (payerId in payers) {
+          totalExpense = totalExpense + payers[payerId].cost;
+        }
+      }
+
+      return totalExpense;
+    };
   }
 ]);
