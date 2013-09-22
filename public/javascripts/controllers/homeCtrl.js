1ba92e211614d65ce523e7c408bb4a3456fa20a3
Owen Chu <owenchu@gmail.com>
2013-09-22 01:44:33 -0700

    Dump data to home page.

--- public/javascripts/controllers/homeCtrl.js
+++ public/javascripts/controllers/homeCtrl.js
@@ -1,7 +1,15 @@
 'use strict';
 
-travelpalApp.controller('homeCtrl', ['$scope',
-  function($scope) {
+travelpalApp.controller('homeCtrl', ['$scope', 'angularFire', 'firebaseRef',
+  function($scope, angularFire, firebaseRef) {
     console.log('homeCtrl');
+
+    $scope.events = {};
+    $scope.travels = {};
+    $scope.users = {};
+
+    angularFire(firebaseRef.events, $scope, 'events');
+    angularFire(firebaseRef.travels, $scope, 'travels');
+    angularFire(firebaseRef.users, $scope, 'users');
   }
 ]);
