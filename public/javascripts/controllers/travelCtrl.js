de05f44774a772c3ee7d0e6fb117687182b9eb82
clydeli <clyde1008li@gmail.com>
2013-09-21 20:53:29 -0700

    add event card draft template

--- public/javascripts/controllers/travelCtrl.js
+++ public/javascripts/controllers/travelCtrl.js
@@ -9,8 +9,18 @@ travelpalApp.controller('travelCtrl', ['$scope', '$routeParams', 'angularFire', 
     $scope.events = {};
     $scope.users = {};
 
+    $scope.getTotalExpense = function(){
+      var total = 0;
+      //for(var event in $scope.events){
+
+      //}
+      return total;
+    };
+
     angularFire(firebaseRef.events, $scope, 'events');
     angularFire(firebaseRef.travel($scope.travelId), $scope, 'travel');
     angularFire(firebaseRef.users, $scope, 'users');
+
+    $scope.expenseSum = $scope.events
   }
 ]);
