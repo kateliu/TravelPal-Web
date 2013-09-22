20269cd2bbebc15af8fd8aa931d9d9cea4d6a423
Lydian Lee <lydian@cmu.edu>
2013-09-22 00:10:55 -0700

    find open travel

--- routes/users.js
+++ routes/users.js
@@ -29,7 +29,32 @@ exports.listTravels = function( req, res) {
 };
 
 exports.getOpenTravel = function( req, res ) {
-  var userID = req.params.id;
+  var userName = req.params.name;
+  var currentUserID = null;
+  new Firebase("https://travelpal.firebaseio.com/users").once("value", function( snapshot ) {
+    var users = snapshot.val(); 
+    for(var userID in users){
+      var user = users[userID];
+      console.log(user);
+      if(user.name == userName) currentUserID = userID;
+    }
+    if(currentUserID == null) { 
+      res.status(404).send("cannot create travel with undefined user");
+      return;
+    }
+    var travelsRef = new Firebase("https://travelpal.firebaseio.com/travels").once("value", function (snapshot) {
+      var currentTimestamp = new Date().getTime();
+      var travels = snapshot.val();
 
+      var openTravel = _und.find(travels , function(travel, travelID) {
+        travel.id = travelID; 
+        travel.events = _und.map(travels, function(evt, eventID) {
+          return eventID;  
+        });
+        return ('time' in travel && (travel.time.length == 1 || travel.time[2] >= currentTimestamp));  
+      });
+      res.json(openTravel);
+    });
+  });
 };
 
