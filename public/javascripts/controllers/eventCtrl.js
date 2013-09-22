3b006132a682c13a62916ee4a1349af0fe642704
Owen Chu <owenchu@gmail.com>
2013-09-21 21:02:18 -0700

    Dump data to event page.

--- public/javascripts/controllers/eventCtrl.js
+++ public/javascripts/controllers/eventCtrl.js
@@ -1,7 +1,19 @@
 'use strict';
 
-travelpalApp.controller('eventCtrl', ['$scope',
-  function($scope) {
-    console.log('eventCtrl');
+travelpalApp.controller('eventCtrl', ['$scope', '$routeParams', 'angularFire', 'firebaseRef',
+  function($scope, $routeParams, angularFire, firebaseRef) {
+    console.log('eventCtrl: ' + $routeParams.eventId);
+
+    var eventId = $routeParams.eventId;
+
+    $scope.event = {};
+    $scope.travel = {};
+    $scope.users = {};
+
+    angularFire(firebaseRef.event(eventId), $scope, 'event').then(function() {
+      angularFire(firebaseRef.travel($scope.event.travel), $scope, 'travel');
+    });
+
+    angularFire(firebaseRef.users, $scope, 'users');
   }
 ]);
