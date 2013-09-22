140d125feade7e653ed923d2161a298b35cb4a13
Lydian Lee <lydian@cmu.edu>
2013-09-21 18:48:57 -0700

    show all events in travel

--- routes/travels.js
+++ routes/travels.js
@@ -2,6 +2,7 @@
 //Setup for firebase
 var Firebase = require('firebase');
 var firebaseRootRef = new Firebase('https://travelpal.firebaseio.com/travels');
+
 exports.create = function (req, res) {
   var newTravel = req.body;
   var id = firebaseRootRef.push(newTravel).name();
@@ -19,5 +20,16 @@ exports.list = function (req, res) {
 };
 
 exports.listEvents = function (req, res) {
-  res.json({param: req.params.id}); 
+  var travelID = req.params.id;
+  console.log(travelID);
+  var travelRef = new Firebase(firebaseRootRef.child(travelID).toString());
+  travelRef.once("value", function(snapshot) {
+    if(snapshot.val() == null) {
+      res.status(404).send("404 Travel Not Found");
+    }
+    else{
+      res.json(snapshot.val());
+    } 
+  });
+
 };
